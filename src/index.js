0202cf87bb370cf25c8cd2bb468e0f588a714c75
sjcg <zsjcgfd@hotmail.com>
2021-05-18 15:27:50 +0800

    init project
    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+function component(){
+	const ele= document.createElement('div');
+	ele.innerHTML = _.join(['hello','webpack'],' ');
+	return ele;
+}
+
+document.body.appendChild(omponent());
